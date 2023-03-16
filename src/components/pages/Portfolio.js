a132b7eda31622632e12b848513a706e69afd06d
Julian Mason <julian@Julians-MacBook-Air-2.local>
2023-03-16 15:51:58 -0700

    added project 3

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,14 +2,14 @@ import React from "react";
 import '../../styles/Style.css';
 import Project from '../project';
 import Movie from '../../styles/cinema-sips.png';
+import Clash from '../../styles/picsRus.png';
 
 export default function Portfolio() {
     return (
         <div className='mainBody'>
         <h1 className="title">Portfolio</h1>
         <Project title="Sips & Cinema (group project)" pic={Movie} alt="Project Pic" details="Generate a random movie and drink of your choice." link="https://kawaikimono.github.io/Movies-n-Drinks/"/>
-        <Project title="Tech Blog" details="Users can post their blogs and comment on others" link=" https://techblog-j.herokuapp.com/"/>
-            <Project title="pics R us" details="Users can post/interact with pictures" link="https://picrus.netlify.app/?gallery=technology"/>
+        <Project title="pics R us (group project)" pic={Clash} alt="Project Pic" details="Users can post/interact with pictures" link="https://picrus.netlify.app/?gallery=technology"/>
         </div>
     )
 }
